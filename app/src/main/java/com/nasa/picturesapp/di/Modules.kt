b51c633a496e76f5fadc025ac31a15093fa5dbc8
Milind Mevada <milindmevada@gmail.com>
2021-03-05 22:34:04 +0530

    Created Basic structure for dependencies.

---
+++ app/src/main/java/com/nasa/picturesapp/di/Modules.kt
@@ -0,0 +1,19 @@
+package com.nasa.picturesapp.di
+
+import org.koin.dsl.module
+
+val applicationModule = module {
+    //Application Level dependencies goes here
+}
+
+val serviceModules = module {
+    //Service Layer dependencies goes here
+}
+
+val repositoryModules = module {
+    //Repositories dependencies goes here
+}
+
+val viewModelModules = module {
+    //ViewModels dependencies goes here
+}
